906ce7df50855c4f08c508e475f8cbb3eab99f3b
Will Farmerie <will.farmerie@gmail.com>
2020-03-27 12:09:16 -0600

    mongodb hardcode

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config() 
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+mongoose.connect('mongodb://eliveadmin:Farmerie12@ds227664.mlab.com:27664/heroku_pz69pc25', { useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('connected to database'));
