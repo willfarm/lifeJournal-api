032d06d28ba5727336c101723b00b15ea53d0637
Will Farmerie <will.farmerie@gmail.com>
2020-07-02 09:43:49 -0600

    forgot password

--- models/email.helper.js
+++ models/email.helper.js
@@ -1,12 +1,5 @@
 const nodemailer = require("nodemailer");
 
-const config = new AWS.Config({
-  region: "us-east-1",
-  secretAccessKey: process.env.SECRET,
-  accessKeyId: process.env.KEY_ID,
-});
-const ses = new AWS.SES(config);
-
 function sendResetLink(email, id) {
   let transporter = nodemailer.createTransport({
     service: "gmail",
@@ -24,30 +17,6 @@ function sendResetLink(email, id) {
   };
 
   transporter.sendMail(mailOptions, function (err, data) {
-    if (err) {
-      console.log(err);
-    }
-  });
-  const params = {
-    Destination: {
-      ToAddresses: [email],
-    },
-    Message: {
-      Body: {
-        Text: {
-          Charset: "UTF-8",
-          Data: `To reset your password, please click on this link: http://localhost:3000/reset/${id}`,
-        },
-      },
-      Subject: {
-        Charset: "UTF-8",
-        Data: "Reset password instructions",
-      },
-    },
-    Source: "noreply@elive.com",
-  };
-
-  ses.sendEmail(params, (err) => {
     if (err) {
       console.log(err);
     }
