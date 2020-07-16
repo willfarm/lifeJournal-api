1b94b2b172fa49be8d2fea84afe95eb36c746955
Will Farmerie <will.farmerie@gmail.com>
2020-07-16 11:03:24 -0600

    change to insurance

--- models/provider.model.js
+++ models/provider.model.js
@@ -1,66 +1,66 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ProviderSchema = new mongoose.Schema({
-	  name: {
-    	type: String,
-    	required: true
-  	},
-  	password: {
-  		type: String,
-  		required: true
-  	},
-  	clinicName: {
-    	type: String,
-    	required: true
-  	},
-		email: {
-			type: String,
-			required: false
-		},
-  	address: {
-    	type: String,
-    	required: true
-  	},
-  	phoneNumber: {
-    	type: String,
-    	required: true
-  	},
-  	beds: {
-    	type: String,
-    	required: true
-  	},
-  	patients: {
-    	type: String,
-    	required: true
-  	},
-  	insurance: {
-    	type: String,
-    	required: true
-  	},
-		inPatient: {
-    	type: Boolean,
-    	required: true
-  	},
-		outPatient: {
-    	type: Boolean,
-    	required: true
-  	},
-		am: {
-    	type: Boolean,
-    	required: true
-  	},
-		pm: {
-    	type: Boolean,
-    	required: true
-  	},
-		english: {
-    	type: Boolean,
-    	required: true
-  	},
-		spanish: {
-    	type: Boolean,
-    	required: true
-  	},
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  clinicName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: false,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  beds: {
+    type: String,
+    required: true,
+  },
+  patients: {
+    type: String,
+    required: true,
+  },
+  insurance: {
+    type: [String],
+    required: true,
+  },
+  inPatient: {
+    type: Boolean,
+    required: true,
+  },
+  outPatient: {
+    type: Boolean,
+    required: true,
+  },
+  am: {
+    type: Boolean,
+    required: true,
+  },
+  pm: {
+    type: Boolean,
+    required: true,
+  },
+  english: {
+    type: Boolean,
+    required: true,
+  },
+  spanish: {
+    type: Boolean,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('Provider', ProviderSchema);
+module.exports = mongoose.model("Provider", ProviderSchema);
