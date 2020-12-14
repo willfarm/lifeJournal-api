be8895df7974be2226cb7746f77bb60ebc12f6fd
Will Farmerie <will.farmerie@gmail.com>
2020-12-14 11:12:37 -0700

    typo

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get("/", (req, res) => {
 require("./routes/user.routes")(app);
 require("./routes/journal.routes")(app);
 require("./routes/thankfulness.routes")(app);
-require("./routes/teachingNotes.routes.routes")(app);
+require("./routes/teachingNotes.routes")(app);
 
 app.listen(process.env.PORT || 3000, () => console.log("server started"));
