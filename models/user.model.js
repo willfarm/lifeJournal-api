753cab6d55c80d35fb10ab259dfeccc47d3cead7
Will Farmerie <will.farmerie@gmail.com>
2020-12-17 09:26:03 -0700

    typo on biblestudy model

--- models/user.model.js
+++ models/user.model.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const teachingNotesModel = require("./teachingNotes.model");
 const SALT_WORK_FACTOR = 10;
 
-const Journal = require('./journal.model').schema
-const Thankfulness = require('./thankfulness.model').schema
-const BibleStudy = require('./bibleStudy.model').schema
-const DailyRoutine = require('./dailyRoutine.model').schema
-const Prayer = require('./prayer.model').schema
-const TeachingNotes = require('./teachingNotes.model').schema
+const Journal = require("./journal.model").schema;
+const Thankfulness = require("./thankfulness.model").schema;
+const BibleStudy = require("./bibleStudy.model").schema;
+const DailyRoutine = require("./dailyRoutine.model").schema;
+const Prayer = require("./prayer.model").schema;
+const TeachingNotes = require("./teachingNotes.model").schema;
 
 const UserSchema = new mongoose.Schema({
   password: {
@@ -30,25 +30,24 @@ const UserSchema = new mongoose.Schema({
   },
   journals: [Journal],
   thankfulness: [Thankfulness],
-  bibleStudys: [BibleStudy],
+  bibleStudy: [BibleStudy],
   dailyRoutine: [DailyRoutine],
   prayer: [Prayer],
-  teachingNotes: [TeachingNotes]
-
+  teachingNotes: [TeachingNotes],
 });
 
-UserSchema.pre('save', function(next) {
+UserSchema.pre("save", function (next) {
   var user = this;
 
   // only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) return next();
+  if (!user.isModified("password")) return next();
 
   // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
     if (err) return next(err);
 
     // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
+    bcrypt.hash(user.password, salt, function (err, hash) {
       if (err) return next(err);
       // override the cleartext password with the hashed one
       user.password = hash;
@@ -57,12 +56,12 @@ UserSchema.pre('save', function(next) {
   });
 });
 // https://stackoverflow.com/questions/14588032/mongoose-password-hashing
-UserSchema.methods.comparePassword = function(candidatePassword) {
+UserSchema.methods.comparePassword = function (candidatePassword) {
   return new Promise(async (resolve, reject) => {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
       if (err) return reject(err);
       resolve(isMatch);
-    })
+    });
   });
 };
 
