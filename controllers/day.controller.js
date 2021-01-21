d3ceee117de96693b054ca8775966b97fc00df57
Will Farmerie <will.farmerie@gmail.com>
2021-01-21 16:29:05 -0700

    is lifetime

--- controllers/day.controller.js
+++ controllers/day.controller.js
@@ -44,7 +44,7 @@ exports.getDayForUser = (req, res) => {
       .exec()
   );
   promises.push(
-    User.findById(uid).select("subscriptionStatus")
+    User.findById(uid).select("subscriptionStatus, isLifeTimeUser")
       .lean()
       .exec()
   )
